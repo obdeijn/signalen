02940f049b24df20dac59989004be4656a15e309
Jasper Poppe <jgpoppe@gmail.com>
2020-06-03 11:44:49 +0200

    (Fix) Show right Jira URL and questions formatting

--- packages/siali/src/flags.ts
+++ packages/siali/src/flags.ts
@@ -2,35 +2,28 @@ import {flags} from '@oclif/command'
 
 import {cli, IPromptOptions} from 'cli-ux'
 
+import chalk from 'chalk'
+
 import configuration from './lib/configuration'
-
-const gitHubTokenInstructions = [
-  'Get personal access GitHub token from here (enable repo access):',
-  '',
-  'https://github.com/settings/tokens/new'
-].join('\n')
-
-const jiraTokenInstructions = [
-  'Get Jira token from here:',
-  '',
-  'https://github.com/settings/tokens/new'
-].join('\n')
 
 const setFlag = async (flagName: string, description: string | undefined, options: IPromptOptions | undefined = undefined) => {
   const configuredValue = configuration.get(flagName)
   if (configuredValue) return configuredValue
 
-  if (description) console.log(description)
-  console.log(`Default ${flagName} is not configured, flag --${flagName} flag is not specified`)
+  console.log(`Default ${chalk.red(flagName)} is not configured, flag --${chalk.green(flagName)} flag is not specified`)
   console.log()
 
-  const value = await cli.prompt(`Configure default ${flagName}`, options)
+  if (description) console.log(description)
+  const value = await cli.prompt(`Default ${flagName} (will be stored in configuration)`, options)
   if (!value) throw new Error(`flag ${flagName} is required...`)
 
   configuration.set(flagName, value)
 
   return value
 }
+
+const gitHubTokenInstructions = 'Get personal access GitHub token from https://github.com/settings/tokens/new'
+const jiraTokenInstructions = 'Get Jira token from https://id.atlassian.com/manage-profile/security/api-tokens'
 
 export const repository = flags.string({description: 'Repository slug'})
 export const setRepository = () => setFlag('repository', undefined, {default: 'Amsterdam/signals-frontend'})
